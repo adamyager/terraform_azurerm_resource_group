09e29a4114691941a855c0991d2b93f6bb68dbdd
adamyager <52081264+adamyager@users.noreply.github.com>
2019-10-17 15:49:11 -0500

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "resource_group_id" {
+  value = "${azurerm_resource_group.res_group.id}"
+}
+output "resource_group_name" {
+  value = "${azurerm_resource_group.res_group.name}"
+}
+output "resource_group_location" {
+  value = "${azurerm_resource_group.res_group.location}"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Resource Group Location"
+  default     = "Central US"
+}
+
+variable "name" {
+  description = "Resource Group Name"
+  default     = "my_resource_group"
+}
